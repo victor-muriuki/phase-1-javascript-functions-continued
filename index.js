b6b41a0853e6c2bb78eddd2797c236124cb30302
+ <+>
2023-10-23 22:37:58 +0300

    i dont undertsand why npm is only passing 3/6

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ function saturdayFun (defau= "roller-skate"){
 }
 saturdayFun()
 saturdayFun("bathe my dog")
+//This Saturday, I want to roller-skate!
+//This Saturday, I want to bathe my dog!
 
 function mondayWork(defaul= "go to the office"){
     console.log (`This Monday, I will ${defaul}`)
 }
 mondayWork()
 mondayWork("work from home")
+//This Monday, I will go to the office
+//This Monday, I will work from home
 
 function outer(greeting, msg = "It's a fine day to learn") {
     const innerFunction = function (name, lang = "Python") {
@@ -18,13 +22,25 @@ function outer(greeting, msg = "It's a fine day to learn") {
     return innerFunction;
   }
   console.log (outer("hello")("student", "javascript"))
+  //hello, student! It's a fine day to learn javascript
+
+
+// function wrapAdjective (def="*"){
+//     const inner = function (special){
+//         return  `You are ${def} ${special}`
+//     }
+//     return inner("a dedicated programer")
+// }
+// console.log (wrapAdjective())
+// console.log (wrapAdjective("%")("a dedicated programer"))
 
 
 function wrapAdjective (def="*"){
     const inner = function (special){
         return  `You are ${def} ${special}`
     }
-    return inner("a dedicated programer")
+    return inner
 }
-console.log (wrapAdjective())
-console.log (wrapAdjective("%"))
+
+console.log (wrapAdjective("%")("a dedicated programer"))
+//You are % a dedicated programer
