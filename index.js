102a19c8393c6e8c6270a013a0b0efc01064c987
+ <+>
2023-10-23 22:11:50 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+
+function saturdayFun (defau= "roller-Skate"){
+    console.log(`This Saturday, I want to ${defau}!`)
+}
+
+saturdayFun("bathe my dog!")
+
+function mondayWork(defaul= "go to the office"){
+    console.log (`This Monday, I will ${defaul}`)
+}
+
+mondayWork("work from home")
+
+function outer(greeting, msg = "It's a fine day to learn") {
+    const innerFunction = function (name, lang = "Python") {
+      return `${greeting}, ${name}! ${msg} ${lang}`;
+    };
+    return innerFunction;
+  }
+  console.log (outer("hello")("student", "javascript"))
+
+
+function wrapAdjective (def="*"){
+    const inner = function (special){
+        return  `You are ${def} ${special}`
+    }
+    return inner()
+}
+wrapAdjective(("%")("a dedicated programmer"))
